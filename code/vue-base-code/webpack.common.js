0420e93f8ed9341f3ae3957e473066f263f26c21
ztzh-xuyp <ztzh_xuyp@ztzh.com>
2020-12-17 17:50:55 +0800

    配置dev Server

--- code/vue-base-code/webpack.common.js
+++ code/vue-base-code/webpack.common.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'none',
   entry: './src/main.js',
+  publicPath: './',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
@@ -53,6 +55,10 @@ module.exports = {
   },
   plugins: [
     // 将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      url: __dirname
+    })
   ]
 }
